cc3c3a8a3f247804b9246eb1ab1719e931b33a8b
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-03-31 22:01:27 +0800

    Add Templates directory and python template

---
+++ vim/Templates/=template=.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3 -u
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) %USER%.
+#
+# E-Mail: <%MAIL%>
+# %YEAR%-%MONTH%-%DAY% %TIME%
+#
+# This source code is licensed under the %LICENSE% license found in the
+# LICENSE file in the root directory of this source tree.
