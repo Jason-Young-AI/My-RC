9df7159453016ccb366574436ed8346733fd3db5
Jason-Young-AI <AI.Jason.Young@outlook.com>
2020-10-20 21:32:14 +0800

    Update Template

---
+++ vim/Templates/=template=.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Copyright (c) %USER%.
+#
+# E-Mail: <%MAIL%>
+# %YEAR%-%MONTH%-%DAY% %TIME%
+#
+# This source code is licensed under the %LICENSE% license found in the
+# LICENSE file in the root directory of this source tree.
